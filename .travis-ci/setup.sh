b7ef488a3f522020a736ca2a27624af5193acda7
Marco Aurélio da Silva <marcoonroad@gmail.com>
2017-08-31 15:51:02 -0300

    shell script sucks

--- .travis-ci/setup.sh
+++ .travis-ci/setup.sh
@@ -1,20 +1,20 @@
 # Travis-CI custom script
 
-if [[ ( -d $HOME/.travis-ci-lua/$LUA_BIN-$LUA_VERSION/bin ) && ( $REBUILD != "1" ) ]]
+if [[ ( -d $HOME/.travis-ci-lua/lua$LUA_SUFFIX-$LUA_VERSION/bin ) && ( $REBUILD != "1" ) ]]
 then
     echo ""
     echo "==========================================================="
-    echo "*** Reusing cache directory $HOME/.travis-ci-lua/$LUA_BIN-$LUA_VERSION..."
+    echo "*** Reusing cache directory $HOME/.travis-ci-lua/lua$LUA_SUFFIX-$LUA_VERSION..."
     echo "==========================================================="
     echo ""
 else
     mkdir -p $HOME/.travis-ci-lua
-    mkdir -p $HOME/.travis-ci-lua/$LUA_BIN-$LUA_VERSION
+    mkdir -p $HOME/.travis-ci-lua/lua$LUA_SUFFIX-$LUA_VERSION
     CURRENT_DIRECTORY=`pwd`
-    cd $HOME/.travis-ci-lua/$LUA_BIN-$LUA_VERSION
+    cd $HOME/.travis-ci-lua/lua$LUA_SUFFIX-$LUA_VERSION
 
-    case $LUA_BIN in
-        "lua")
+    case $LUA_SUFFIX in
+        "")
             echo ""
             echo "==========================================================="
             echo "*** Building lua..."
@@ -24,12 +24,13 @@ else
             make linux test
             # make install INSTALL_TOP=$HOME/.travis-ci-lua/$LUA_BIN-$LUA_VERSION/install # make local
             make install INSTALL_TOP=`pwd`/install
+            export LUA_INCLUDE_DIR=`pwd`/install/include
             cd ..
             echo "*** Lua is built!"
             echo "==========================================================="
             echo "";;
 
-        "luajit")
+        "jit")
             echo "============================================================"
             echo "*** Building LuaJIT..."
             wget http://luajit.org/download/LuaJIT-$LUA_VERSION.tar.gz
@@ -39,7 +40,8 @@ else
             mkdir -p install
             make
             make install PREFIX=`pwd`/install
-            ln -sfv install/bin/luajit-$LUA_VERSION install/bin/lua
+            ln -sfv install/bin/luajit-$LUA_VERSION install/bin/luajit
+            export LUA_INCLUDE_DIR=`pwd`/install/include/luajit-2.0
             cd ..
             # ln -sfv -T `pwd`/LuaJIT-$LUA_VERSION `pwd`/luajit-$LUA_VERSION
             echo "*** LuaJIT is built!"
@@ -48,7 +50,7 @@ else
 
         "*")
             echo "================================================[ ERROR ]==="
-            echo "*** Invalid lua interpreter called $LUA_BIN!"
+            echo "*** Invalid lua interpreter suffix $LUA_SUFFIX!"
             echo "============================================================"
             exit -1;;
     esac
@@ -62,8 +64,10 @@ else
     wget https://www.luarocks.org/releases/luarocks-$LUAROCKS_VERSION.tar.gz
     tar zxpf luarocks-$LUAROCKS_VERSION.tar.gz
     cd luarocks-$LUAROCKS_VERSION
-    ./configure --with-lua=$CACHE_DIR/$LUA_BIN-$LUA_VERSION/install \
-        --prefix=$CACHE_DIR/$LUA_BIN-$LUA_VERSION/install
+    ./configure --with-lua=$CACHE_DIR/lua$LUA_SUFFIX-$LUA_VERSION/install \
+                  --prefix=$CACHE_DIR/lua$LUA_SUFFIX-$LUA_VERSION/install \
+              --lua-suffix=$LUA_SUFFIX                                    \
+        --with-lua-include=$LUA_INCLUDE_DIR
     make build
     make install
     cd ..
@@ -76,11 +80,11 @@ else
     echo ""
     echo "==========================================================="
     echo "*** Linking directories..."
-    ln -sfv -t $CACHE_DIR $CACHE_DIR/$LUA_BIN-$LUA_VERSION/install/bin     \
-                          $CACHE_DIR/$LUA_BIN-$LUA_VERSION/install/lib     \
-                          $CACHE_DIR/$LUA_BIN-$LUA_VERSION/install/include \
-                          $CACHE_DIR/$LUA_BIN-$LUA_VERSION/install/share   \
-                          $CACHE_DIR/$LUA_BIN-$LUA_VERSION/install/man
+    ln -sfv -t $CACHE_DIR $CACHE_DIR/lua$LUA_SUFFIX-$LUA_VERSION/install/bin     \
+                          $CACHE_DIR/lua$LUA_SUFFIX-$LUA_VERSION/install/lib     \
+                          $CACHE_DIR/lua$LUA_SUFFIX-$LUA_VERSION/install/include \
+                          $CACHE_DIR/lua$LUA_SUFFIX-$LUA_VERSION/install/share   \
+                          $CACHE_DIR/lua$LUA_SUFFIX-$LUA_VERSION/install/man
     cd $CURRENT_DIRECTORY
     echo "*** Linked directories!"
     echo "==========================================================="
@@ -90,11 +94,11 @@ fi
 echo ""
 echo "==============================================================="
 echo "*** Setting up environment..."
-PATH=$HOME/.travis-ci-lua/$LUA_BIN-$LUA_VERSION/bin:$PATH
+PATH=$HOME/.travis-ci-lua/lua$LUA_SUFFIX-$LUA_VERSION/bin:$PATH
 export PATH
-LD_LIBRARY_PATH=$HOME/.travis-ci-lua/$LUA_BIN-$LUA_VERSION/lib:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=$HOME/.travis-ci-lua/lua$LUA_SUFFIX-$LUA_VERSION/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
-MANPATH=$HOME/.travis-ci-lua/$LUA_BIN-$LUA_VERSION/man:$MANPATH
+MANPATH=$HOME/.travis-ci-lua/lua$LUA_SUFFIX-$LUA_VERSION/man:$MANPATH
 export MANPATH
 echo "*** Configuration is done!"
 echo "==============================================================="
